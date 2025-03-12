82f97d9b04900894cf76b2f838a4c1a62d542406
Theodore Brockman <iam@theo.lol>
2025-03-12 09:51:30 -0400

    Start re-writing test.

--- tests/backend/single-buffer.test.ts
+++ tests/backend/single-buffer.test.ts
@@ -1,6 +1,8 @@
 import test, { suite } from 'node:test';
 import { fs as _fs, mount, resolveMountConfig, SingleBuffer, umount } from '../../dist/index.js';
 import assert from 'node:assert';
+import { dirname } from '../../dist/path.js';
+import { fs } from '../common.js';
 
 await suite('SingleBuffer`', () => {
 	test('should be able to restore filesystem (with same metadata) from original buffer', async () => {
@@ -10,15 +12,52 @@ await suite('SingleBuffer`', () => {
 		const writable = await resolveMountConfig({ backend: SingleBuffer, buffer });
 		mount('/', writable);
 
-		_fs.writeFileSync('/example.ts', 'console.log("hello world")', 'utf-8');
-		const stats = _fs.statSync('/example.ts');
+		let files = [
+			{
+				path: '/example.ts',
+				content: 'console.log("hello world")',
+				mode: 0o644,
+				uid: 0,
+				gid: 0,
+			},
+			{
+				path: '/example/nested/folder/nested.ts',
+				content: 'console.log("hello world")'.repeat(1024),
+				mode: 0o644,
+				uid: 0,
+				gid: 0,
+			},
+			{
+				path: '/example/elsewhere/a/symlink',
+				target: '/example.ts',
+			},
+		];
+
+		for (const { path, content, mode, uid, gid, target } of files) {
+			_fs.mkdirSync(dirname(path), { recursive: true });
+
+			if (target) {
+				_fs.symlinkSync(target, path);
+			} else {
+				_fs.writeFileSync(path, content!, { mode, encoding: 'utf-8' });
+				_fs.chmodSync(path, mode!);
+				_fs.chownSync(path, uid!, gid!);
+			}
+		}
+
+		// @ts-ignore
+		files = files.map(({ path, content, mode, uid, gid }) => {
+			const stats = _fs.lstatSync(path);
+			const written = _fs.readFileSync(path, 'utf-8');
+			assert.strictEqual(written, content, `Expected ${path} to have content "${content}"`);
+			mode && assert.strictEqual(stats.mode, mode, `Expected ${path} to have mode ${mode.toString(8)}`);
+			uid && assert.strictEqual(stats.uid, uid, `Expected ${path} to have uid ${uid}`);
+			gid && assert.strictEqual(stats.gid, gid, `Expected ${path} to have gid ${gid}`);
+			return { path, content, stats };
+		});
 
 		umount('/');
 		const snapshot = await resolveMountConfig({ backend: SingleBuffer, buffer });
 		mount('/', snapshot);
-
-		const snapshotStats = _fs.statSync('/example.ts');
-
-		assert.deepEqual(snapshotStats, stats);
 	});
 });
